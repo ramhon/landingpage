66a92289600a3ef10ba15d841c5b1baad9525859
ramhon <85329137+ramhon@users.noreply.github.com>
2025-03-14 16:12:21 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -60,7 +60,7 @@ function App() {
         <a href="https://www.instagram.com/ramhon" className="text-white/70 hover:text-white transition-colors">
           <Instagram size={20} />
         </a>
-        <a href="https://www.twitter.com" className="text-white/70 hover:text-white transition-colors">
+        <a href="https://www.ramhon.com.br/contato" className="text-white/70 hover:text-white transition-colors">
           <MessageCircleMore size={20} />
         </a>
       </div>
