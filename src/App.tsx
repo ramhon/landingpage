eb04fa5356c134cc4fc7a9a1d589e9a871a3d6b1
ramhon <85329137+ramhon@users.noreply.github.com>
2025-03-18 19:28:09 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -75,7 +75,7 @@ function App() {
           }}
         />
         
-        <div className="absolute inset-0 bg-black/50 z-10" />
+       // <div className="absolute inset-0 bg-black/50 z-10" />
 
         <div className="relative z-20 text-right mr-20">
                    
